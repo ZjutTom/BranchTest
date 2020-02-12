58b2263b68ce6b27c0a5993b7270193b0b533fd6
ZjutTom <1499654676@qq.com>
2020-02-12 12:06:10 +0800

    '第一次提交'

---
+++ src/main/java/com/myhexin/branchtest/BranchtestApplication.java
@@ -0,0 +1,13 @@
+package com.myhexin.branchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchtestApplication.class, args);
+    }
+
+}
