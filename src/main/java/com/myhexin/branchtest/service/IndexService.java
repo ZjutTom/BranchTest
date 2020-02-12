39fecccfdd1c26ce0cf478f0bb02c757a800f7c4
ZjutTom <1499654676@qq.com>
2020-02-12 12:43:57 +0800

    'dev2第一次提交'

--- src/main/java/com/myhexin/branchtest/service/IndexService.java
+++ src/main/java/com/myhexin/branchtest/service/IndexService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class IndexService {
     public String index() {
-        return "Hello Spring";
+        return "Hello Spring Boot";
     }
 }
